01395bbd57ff3f75071076b03b616ef9af0a5f63
JAEHALEE26 <jhl647@hanyang.ac.kr>
2021-04-01 09:21:52 +0900

    Initial Commit

---
+++ projectOh_02/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  projectOh_02
+//
+//  Created by Jae ha Lee on 2021/04/01.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
